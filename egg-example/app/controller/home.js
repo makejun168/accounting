e5cbacae556b32ad2ec3d5ea179b6af4fcc555ef
poloma <873800030@qq.com>
2023-04-24 17:19:22 +0800

    feat: 数据库信息提交

--- egg-example/app/controller/home.js
+++ egg-example/app/controller/home.js
@@ -5,7 +5,98 @@ const { Controller } = require('egg');
 class HomeController extends Controller {
   async index() {
     const { ctx } = this;
-    ctx.body = 'hi, egg';
+    // const { id } = ctx.query;
+    // ctx.body = id;
+
+    await ctx.render('index.html', {
+      title: '我是马可骏', // 将 title 传入 index.html
+    });
+  }
+
+  // async user() {
+  //   const { ctx } = this;
+  //   const { id } = ctx.params; // 通过 Params 参数获取数据
+  //   ctx.body = id;
+  // }
+
+  // async user() {
+  //   const { ctx } = this;
+  //   const { name, slogen } = await ctx.service.home.user();
+  //   ctx.body = {
+  //     name,
+  //     slogen
+  //   }
+  // }
+
+  async user() {
+    const { ctx } = this;
+    const result = await ctx.service.home.user();
+    ctx.body = result;
+  }
+
+  async add() {
+    const { ctx } = this;
+    const { title } = ctx.request.body;
+    ctx.body = {
+      title
+    };
+  }
+
+  async editUser() {
+    const { ctx } = this;
+    const { id, name } = ctx.request.body;
+    try {
+      const result = await ctx.service.home.editUser({id, name});
+      ctx.body = {
+        code: 200,
+        msg: '修改成功',
+        data: null
+      }
+    } catch (error) {
+      ctx.body = {
+        code: 500,
+        msg: '修改失败',
+        data: null
+      }
+    }
+  }
+
+  async deleteUser() {
+    const { ctx } = this;
+    const { id } = ctx.request.body;
+    try {
+      const result = await ctx.service.home.deleteUser(id);
+      ctx.body = {
+        code: 200,
+        msg: '删除成功',
+        data: null
+      }
+    } catch (err) {
+      ctx.body = {
+        code: 500,
+        msg: '删除失败',
+        data: null
+      }
+    }
+  }
+
+  async addUser() {
+    const { ctx } = this;
+    const { name } = ctx.request.body;
+    try {
+      const result = await ctx.service.home.addUser(name);
+      ctx.body = {
+        code: 200,
+        msg: '添加成功',
+        data: null
+      }
+    } catch (error) {
+      ctx.body = {
+        code: 500,
+        msg: '添加失败',
+        data: null
+      }
+    }
   }
 }
 
--- egg-example/config/config.default.js
+++ egg-example/config/config.default.js
@@ -18,6 +18,38 @@ module.exports = appInfo => {
   // add your middleware config here
   config.middleware = [];
 
+  config.security = {
+    csrf: {
+      enable: false,
+      ignoreJSON: true
+    },
+    domainWhiteList: ['*']
+  }
+
+  config.view = {
+    mapping: {'.html': 'ejs'}  //左边写成.html后缀，会自动渲染.html文件
+  };
+
+  config.mysql = {
+    // 单数据库信息配置
+    client: {
+      // host
+      host: 'localhost',
+      // 端口号
+      port: '3306',
+      // 用户名
+      user: 'root',
+      // 密码
+      password: '', // 初始化密码，没设置的可以不写
+      // 数据库名
+      database: 'juejue-cost', // 我们新建的数据库名称
+    },
+    // 是否加载到 app 上，默认开启
+    app: true,
+    // 是否加载到 agent 上，默认关闭
+    agent: false,
+  }
+
   // add your user config here
   const userConfig = {
     // myAppName: 'egg',
